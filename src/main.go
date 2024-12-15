eefc153c49acade9e25bc62c12c89d8a20705238
Bailee Kingston <baileek@pm.me>
2024-12-15 22:48:31 +1000

    rback to basics for index.html for now, replace GORM with raw SQL

---
+++ src/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// "io"
+// "html/template"
+// "log"
+// "net/http"
+
+func main() {
+
+	// h1 := func(w http.ResponseWriter, r *http.Request) {
+	// 	template := template.Must(template.ParseFiles("index.html"))
+	// 	template.Execute(w, nil)
+	// }
+	// http.HandleFunc("/", h1)
+	// log.Fatal(http.ListenAndServe(":8000", nil))
+	db := initDbConn()
+	fmt.Sprint(db)
+	// insert(db)
+	setupRoutes()
+}
---
+++ src/models.go
@@ -0,0 +1,11 @@
+package main
+
+// type Users struct {
+// 	gorm.Model
+// 	queries []Queries
+// }
+
+// type Queries struct {
+// 	gorm.Model
+// 	query_img string
+// }
